b80be066203e8018b953fdf3a6c1a7ae5587d738
seaAdmin <30364557+seaphper@users.noreply.github.com>
2018-01-20 13:56:52 +0800

    Add files via upload

---
+++ config/constants.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'SYSTEM_ADDRESS' => 'http://192.168.3.21/sea/',
+    'SYSTEM_NAME' => 'SEA-后台管理系统',
+    'SYSTEM_TITLE' => 'SEA-后台管理系统',
+    'SYSTEM_BUTTON' => '© hojock.com - SEA-后台管理系统 '
+];
